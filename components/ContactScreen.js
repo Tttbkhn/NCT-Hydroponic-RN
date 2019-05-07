020dd2e97d935be0d91a4cbd4b8004942a7ede29
tttbkhn <tttbkhn@gmail.com>
2019-05-07 11:51:24 +0700

    add maps in contact and change pH gauge

--- components/ContactScreen.js
+++ components/ContactScreen.js
@@ -1,12 +1,21 @@
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
+import { MapView } from 'expo';
 
 export default class ContactScreen extends Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Contact</Text>
-        </View>
-      );
-    }
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        provider="google"
+        region={{
+          latitude: 40.76727216,
+          longitude: -73.99392888,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
+    );
   }
+}
--- components/Info/PHScreen.js
+++ components/Info/PHScreen.js
@@ -16,13 +16,23 @@ export default class PHScreen extends Component {
       series: [{
         name: 'Do pH',
         type: 'gauge',
+        min: 0,
+        max: 14,
+        splitNumber: 14,
+        radius: '90%',
+        axisLine: {
+          lineStyle: {
+            color: [[0.285, 'lime'], [0.714, '#1e90ff'], [1, '#ff4500']],
+            width: 25,
+          }
+        },
         detail: { formatter: '{value}%' },
-        data: [{ value: 70, name: 'pH' }]
+        data: [{ value: 7, name: 'pH' }]
       }]
     };
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Echarts option={option} height={300} />
+        <Echarts option={option} height={500} />
       </View>
     );
   }
